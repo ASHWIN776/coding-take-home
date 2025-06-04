26667660f0ee95535698339abe92a7ddf421a853
Ashwin Anil <ashwinrockon123@gmail.com>
2025-06-04 20:58:29 +0530

    feat: Added fetch calls in middleware.ts
    3 fetch calls added:
    - Return an array of listings of a particular color or language
    - Return an array of listings of all countries represented in the database
    - Return an array of all listings which have a null value of a particular key like color or language or country

--- take-home/src/middleware/middleware.ts
+++ take-home/src/middleware/middleware.ts
@@ -2,5 +2,24 @@ import data from '../mock-data/MOCK_DATA.json';
 
 // This is a tester function to show the intended flow of data from middleware to frontend.
 export const fetchData = () => {
-	console.log(data)
+	return data;
 };
+
+export const fetchByProperty = (property: 'color' | 'language', value: string) => {
+	return data.filter((item) => item[property] === value)
+}
+
+export const fetchCountryData = () => {
+	return data.reduce((acc, item) => {
+		if (item.country) {
+			acc[item.country] = acc[item.country] || [];
+			acc[item.country].push(item);
+		}
+		return acc;
+	}, {} as Record<string, typeof data>)
+}
+
+export const fetchNullData = (property: 'color' | 'language' | 'country') => {
+	return data.filter((item) => item[property] === null)
+}
+
